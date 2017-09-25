2494a6dfed112f0087ed3047613515b683f1558e
drkalavera <808khz@gmail.com>
2017-09-25 16:57:05 -0500

    primera subida
    KEKSTER 2000

---
+++ Prueba bing/Prueba bing/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Net.Http;
+using System.Web;
+
+namespace CSHttpClientSample
+{
+    static class Program
+    {
+        static void Main()
+        {
+            MakeRequest();
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        static async void MakeRequest()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "64f004f772a540ab92636c548eafff76");
+
+            // Request parameters
+            queryString["q"] = "dog";
+            queryString["count"] = "10";
+            queryString["offset"] = "0";
+            queryString["mkt"] = "en-us";
+            queryString["safesearch"] = "Moderate";
+            var uri = "https://api.cognitive.microsoft.com/bing/v5.0/search?" + queryString;
+
+            //URL
+            Console.WriteLine("+Requested URL----------------------------------------------------");
+            Console.WriteLine(uri);
+            Console.WriteLine("\n");
+            HttpResponseMessage response = await client.GetAsync(uri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("+Response    ----------------------------------------------------");
+            Console.WriteLine(responseBody);
+            Console.WriteLine("\n");
+        
+        }
+    }
+}
