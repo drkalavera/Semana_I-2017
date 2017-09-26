48310619505dadbd8cf4a305bdb6d06c5b79729e
drkalavera <808khz@gmail.com>
2017-09-26 15:31:47 -0500

    sss
    sss

--- Prueba bing/Prueba bing/Program.cs
+++ Prueba bing/Prueba bing/Program.cs
@@ -7,6 +7,7 @@ using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
+#region Referencias
 /*
  Referencias utilizadas :
 https://dev.cognitive.microsoft.com/docs/services/56b43eeccf5ff8098cef3807/operations/56b4447dcf5ff8098cef380d 
@@ -17,6 +18,7 @@ https://stackoverflow.com/questions/12676746/parse-json-string-in-c-sharp
 https://stackoverflow.com/questions/40416569/newtonsoft-could-not-be-found
 http://www.c-sharpcorner.com/blogs/first-letter-in-uppercase-in-c-sharp1 
  */
+#endregion
 
 namespace CSHttpClientSample
 {
@@ -28,6 +30,8 @@ namespace CSHttpClientSample
             Console.WriteLine("Hit ENTER to exit...");
             Console.ReadLine();
         }
+
+        #region APPs
 
         public static string FirstCharToUpper(string s)
         {
@@ -46,16 +50,16 @@ namespace CSHttpClientSample
         {
             // Imprimir WEBPAGES   
              datum("WEBPAGES");
-            imprimirCosas(stuff.webPages.value);
+            imprimirCosas(stuff.webPages.value,1);
             //iMPRIMIR IMAGES
             datum("Images");
-            imprimirCosas(stuff.images.value);
+            imprimirCosas(stuff.images.value,2);
             //IMPRIMIR RELATEDSEARCHES
             datum("RELATEDSEARCHES");
-            imprimirCosas(stuff.relatedSearches.value);
+            imprimirCosas(stuff.relatedSearches.value,3);
             //IMPRIMIR VIDEOS
             datum("videos");
-            imprimirCosas(stuff.videos.value);
+            imprimirCosas(stuff.videos.value,4);
         }
 
         static void datum(string ke)
@@ -63,46 +67,6 @@ namespace CSHttpClientSample
             Console.WriteLine("\n***************************************************************************");
             Console.Write(ke);
             Console.WriteLine("\n***************************************************************************");
-        }
-
-        static void imprimirCosas(dynamic stuff)
-        {
-            int i = 0;
-
-            foreach (JObject x in stuff)
-            {
-                int cc = 0; 
-                foreach (KeyValuePair<String, JToken> app in x)
-                {
-                    var appName = app.Key;
-                    var description = app.Value;
-
-                    if (!appName.Equals("dateLastCrawled"))
-                    {
-                        if (cc==0)
-                        {
-                            Console.WriteLine("\n");
-                            Console.Write("Resultado ");
-                            Console.Write(i);
-                            Console.WriteLine(":");
-                            i++;
-                            cc++;
-                        }
-                        Console.WriteLine("-------------------------------------------------------------");
-                        if (appName.Equals("snippet"))
-                        {
-                            Console.Write("About");
-                        }
-                        else
-                        {
-                            Console.Write(FirstCharToUpper(appName));
-                        }
-                        Console.WriteLine(":\n-------------------------------------------------------------");
-                        Console.WriteLine(description);
-                    }
-                    
-                }
-            }
         }
 
         static async void MakeRequest()
@@ -130,10 +94,95 @@ namespace CSHttpClientSample
             dynamic stuff = JsonConvert.DeserializeObject(json);
 
             obtenerDatos(stuff);
-        
+
         }
+
+        #endregion
+
+        #region Impresion
+
+        static void imprimirCosas(dynamic stuff, int Que)
+        {
+            //1-webpages 2-images 3-relatedsearches 4-videos
+            int i = 0;
+            foreach (JObject x in stuff)
+            {
+                int cc = 0;
+                foreach (KeyValuePair<String, JToken> app in x)
+                {
+                    var appName = app.Key;
+                    var description = app.Value;
+                    if (cc == 0)
+                    {
+                        Console.WriteLine("\n");
+                        Console.Write("Resultado ");
+                        Console.Write(i);
+                        Console.WriteLine(":");
+                        i++;
+                        cc++;
+                    }
+                    imprimirQue(Que,appName,description);
+                }
+            }
+        }
+
+        static void imprimirQue(int flag,string dato, JToken desc)
+        {
+            //1-webpages 2-images 3-relatedsearches 4-videos
+            switch (flag)
+            {
+                case 1:
+                    iWebpages(dato, desc);
+                    break;
+                case 2:
+                    iImages(dato,desc);
+                    break;
+                case 3:
+                    iRelatedsearches(dato, desc);
+                    break;
+                case 4:
+                    iVideos(dato, desc);
+                    break;
+                default:
+                    Console.WriteLine(":(");
+                    break;
+            }
+        }
+
+        static void iWebpages(string dato, JToken desc)
+        {
+            if (!dato.Equals("dateLastCrawled"))
+            {
+                Console.WriteLine("-------------------------------------------------------------");
+                if (dato.Equals("snippet"))
+                {
+                    Console.Write("About");
+                }
+                else
+                {
+                    Console.Write(FirstCharToUpper(dato));
+                }
+                Console.WriteLine(":\n-------------------------------------------------------------");
+                Console.WriteLine(desc);
+            }
+        }
+
+        static void iImages(string dato, JToken desc)
+        {
+
+        }
+
+        static void iRelatedsearches(string dato, JToken desc)
+        {
+
+        }
+
+        static void iVideos(string dato, JToken desc)
+        {
+
+        }
+
+        #endregion
+
     }
 }
-
-
-
